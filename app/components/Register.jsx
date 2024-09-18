a6782767b1b854ce61834d09f4770bbce4d6409a
Bhavan S <109290304+parzi-al@users.noreply.github.com>
2024-09-18 21:58:34 +0530

    backend

--- app/components/Register.jsx
+++ app/components/Register.jsx
@@ -1,7 +1,9 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { collection, addDoc } from 'firebase/firestore';
-import db from '../utils/firebase.config.js'; 
+import db from '../utils/firebase.config.js';
+import Image from "next/image";
+
 const Register = () => {
   const [formData, setFormData] = useState({
     firstName: '',
@@ -12,8 +14,11 @@ const Register = () => {
     branchSem: '',
     isIeeeMember: true,
     ieeeId: '',
+    transcationid: '',
   });
-  
+
+  const [showDetails, setShowDetails] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
@@ -23,7 +28,7 @@ const Register = () => {
     }));
   };
 
-  async function addData(firstName, lastName, phoneNumber, email, collegeName, branchSem, isIeeeMember, ieeeId) {
+  async function addData(firstName, lastName, phoneNumber, email, collegeName, branchSem, isIeeeMember, ieeeId,transcationid) {
     try {
       const docRef = await addDoc(collection(db, "CORE"), {
         firstName,
@@ -34,6 +39,7 @@ const Register = () => {
         branchSem,
         isIeeeMember,
         ieeeId,
+        transcationid,
       });
       console.log("Document written with ID: ", docRef.id);
       return true;
@@ -45,90 +51,136 @@ const Register = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { firstName, lastName, phoneNumber, email, collegeName, branchSem, isIeeeMember, ieeeId } = formData;
-    const success = await addData(firstName, lastName, phoneNumber, email, collegeName, branchSem, isIeeeMember, ieeeId);
-    if (success) {
-    console.log('Data added successfully');
+    if (!showDetails) {
+      setIsSubmitted(true);
     } else {
-     console.log('Error adding data',error);
+      console.log('Error adding data');
+    }
+  };
+  const handleSubmit1 = async (e) => {
+    e.preventDefault();
+    const { firstName, lastName, phoneNumber, email, collegeName, branchSem, isIeeeMember, ieeeId, transcationid } = formData;
+    const success = await addData(firstName, lastName, phoneNumber, email, collegeName, branchSem, isIeeeMember, ieeeId, transcationid);
+    if (success) {
+console.log('Data added successfully');
+    } else {
+      console.log('Error adding data');
     }
   };
 
+  useEffect(() => {
+    if (isSubmitted) {
+      setShowDetails(true);
+    }
+  }, [isSubmitted]);
+
   return (
     <div className='text-white flex flex-col gap-7 items-center relative w-3/4'>
-      <div className='w-full'>
-        <div className='text-3xl font-semibold pb-2 text-[#FFFFFFD9]'>Student Details</div>
-        <div className='flex flex-col gap-3 text-[#FFFFFFE5]'>
-          <div className='flex flex-col lg:flex-row gap-3 lg:gap-10'>
-            <div className='flex flex-col lg:w-1/2'>
-              <label className='text-sm pl-4 py-1'>First Name</label>
-              <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} className="input h-10 input-bordered border-2 border-white bg-[#57595d] rounded-full w-full" />
+      {showDetails ? (
+        <div className='w-full flex flex-col items-center'>
+          <div className='text-3xl font-semibold pb-2 text-[#FFFFFFD9]'>Scan the QR code</div>
+          <div className='flex flex-col justify-center gap-3 text-[#FFFFFFE5]'>
+            <div className='flex flex-col justify-center items-center '>
+              <Image src="" width="80" height="90" />
             </div>
-            <div className='flex flex-col lg:w-1/2'>
-              <label className='text-sm  pl-4 py-1'>Last Name</label>
-              <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} className="input h-10 input-bordered border-2 border-white bg-[#57595d] rounded-full w-full" />
-            </div>
-          </div>
+            <div className='flex flex-col justify-center  '>
 
-          <div className='flex flex-col lg:flex-row gap-3 lg:gap-10'>
-            <div className='flex flex-col lg:w-1/2'>
-              <label className='text-sm pl-4 py-1'>Phone Number</label>
-              <input type="text" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} className="input h-10 input-bordered border-2 border-white bg-[#57595d] rounded-full w-full" />
+            <label className='text-sm pl-4 py-1'>Transcation Id</label>
+            <input type="text" name="transcationid" value={formData.transcationid} onChange={handleChange} className="input h-10 input-bordered border-2 border-white bg-[#57595d] rounded-full w-full" />
+            
+            
             </div>
-            <div className='flex flex-col lg:w-1/2'>
-              <label className='text-sm pl-4 py-1'>Email ID</label>
-              <input type="text" name="email" value={formData.email} onChange={handleChange} className="input h-10 input-bordered border-2 border-white bg-[#57595d] rounded-full w-full" />
-            </div>
+            <div className='flex flex-col justify-center  items-center '>
+
+            <button
+              className="btn btn-sm h-9 w-44 flex rounded-full text-white border border-[#505459]"
+              style={{
+                background: `linear-gradient(90deg, rgba(136, 158, 175, 0.8) 0%, rgba(27, 30, 32, 0.744) 98.32%)`,
+              }}
+              onClick={handleSubmit1}
+            >
+              Sign Up
+            </button>
+           </div>
           </div>
         </div>
-      </div>
-
-      <div className='w-full'>
-        <div className='text-3xl font-semibold pb-2 text-[#FFFFFFD9]'>College Details</div>
-        <div className='flex flex-col gap-3'>
-          <div className='flex flex-col lg:flex-row gap-3 lg:gap-10 text-[#FFFFFFE5]'>
-            <div className='flex flex-col lg:w-1/2'>
-              <label className='text-sm pl-4 py-1'>College Name</label>
-              <input type="text" name="collegeName" value={formData.collegeName} onChange={handleChange} className="input h-10 input-bordered border-2 border-white bg-[#57595d] rounded-full w-full" />
-            </div>
-            <div className='flex flex-col lg:w-1/2'>
-              <label className='text-sm pl-4 py-1'>Branch and Semester</label>
-              <input type="text" name="branchSem" value={formData.branchSem} onChange={handleChange} className="input h-10 input-bordered border-2 border-white bg-[#57595d] rounded-full w-full" />
-            </div>
-          </div>
-
-          <div className='flex flex-col lg:flex-row gap-3 lg:gap-10'>
-            <div className='flex flex-col lg:w-1/2 pl-4'>
-              <label className='text-base lg:text-lg  py-1'>Are you an IEEE member?</label>
-              <div className='flex gap-16'>
-                <div className='flex gap-2 text-[#FFFFFFE5]'>
-                  <input type="radio" name="isIeeeMember" value={true} onChange={handleChange} className="radio checked:bg-[#004278] checked:border-0 border-[3px] border-white" defaultChecked /><label>Yes</label>
+      ) : (
+        <>
+          <div className='w-full'>
+            <div className='text-3xl font-semibold pb-2 text-[#FFFFFFD9]'>Student Details</div>
+            <div className='flex flex-col gap-3 text-[#FFFFFFE5]'>
+              <div className='flex flex-col lg:flex-row gap-3 lg:gap-10'>
+                <div className='flex flex-col lg:w-1/2'>
+                  <label className='text-sm pl-4 py-1'>First Name</label>
+                  <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} className="input h-10 input-bordered border-2 border-white bg-[#57595d] rounded-full w-full" />
                 </div>
-                <div className='flex gap-2'>
-                  <input type="radio" name="isIeeeMember" value={false} onChange={handleChange} className="radio checked:bg-[#004278] checked:border-0 border-[3px] border-white" /><label>No</label>
+                <div className='flex flex-col lg:w-1/2'>
+                  <label className='text-sm  pl-4 py-1'>Last Name</label>
+                  <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} className="input h-10 input-bordered border-2 border-white bg-[#57595d] rounded-full w-full" />
+                </div>
+              </div>
+
+              <div className='flex flex-col lg:flex-row gap-3 lg:gap-10'>
+                <div className='flex flex-col lg:w-1/2'>
+                  <label className='text-sm pl-4 py-1'>Phone Number</label>
+                  <input type="text" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} className="input h-10 input-bordered border-2 border-white bg-[#57595d] rounded-full w-full" />
+                </div>
+                <div className='flex flex-col lg:w-1/2'>
+                  <label className='text-sm pl-4 py-1'>Email ID</label>
+                  <input type="text" name="email" value={formData.email} onChange={handleChange} className="input h-10 input-bordered border-2 border-white bg-[#57595d] rounded-full w-full" />
                 </div>
               </div>
             </div>
+          </div>
 
-            <div className='flex flex-col lg:w-1/2'>
-              <label className='text-sm pl-4 py-1 text-[#FFFFFFE5]'>IEEE Membership ID</label>
-              <input type="text" name="ieeeId" value={formData.ieeeId} onChange={handleChange} className="input h-10 input-bordered border-2 border-white bg-[#57595d] rounded-full w-full" />
+          <div className='w-full'>
+            <div className='text-3xl font-semibold pb-2 text-[#FFFFFFD9]'>College Details</div>
+            <div className='flex flex-col gap-3'>
+              <div className='flex flex-col lg:flex-row gap-3 lg:gap-10 text-[#FFFFFFE5]'>
+                <div className='flex flex-col lg:w-1/2'>
+                  <label className='text-sm pl-4 py-1'>College Name</label>
+                  <input type="text" name="collegeName" value={formData.collegeName} onChange={handleChange} className="input h-10 input-bordered border-2 border-white bg-[#57595d] rounded-full w-full" />
+                </div>
+                <div className='flex flex-col lg:w-1/2'>
+                  <label className='text-sm pl-4 py-1'>Branch and Semester</label>
+                  <input type="text" name="branchSem" value={formData.branchSem} onChange={handleChange} className="input h-10 input-bordered border-2 border-white bg-[#57595d] rounded-full w-full" />
+                </div>
+              </div>
+
+              <div className='flex flex-col lg:flex-row gap-3 lg:gap-10'>
+                <div className='flex flex-col lg:w-1/2 pl-4'>
+                  <label className='text-base lg:text-lg  py-1'>Are you an IEEE member?</label>
+                  <div className='flex gap-16'>
+                    <div className='flex gap-2 text-[#FFFFFFE5]'>
+                      <input type="radio" name="isIeeeMember" value={true} onChange={handleChange} className="radio checked:bg-[#004278] checked:border-0 border-[3px] border-white" defaultChecked /><label>Yes</label>
+                    </div>
+                    <div className='flex gap-2'>
+                      <input type="radio" name="isIeeeMember" value={false} onChange={handleChange} className="radio checked:bg-[#004278] checked:border-0 border-[3px] border-white" /><label>No</label>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='flex flex-col lg:w-1/2'>
+                  <label className='text-sm pl-4 py-1 text-[#FFFFFFE5]'>IEEE Membership ID</label>
+                  <input type="text" name="ieeeId" value={formData.ieeeId} onChange={handleChange} className="input h-10 input-bordered border-2 border-white bg-[#57595d] rounded-full w-full" />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
 
-      <div>
-        <div
-          className="btn btn-sm h-9 w-44 rounded-full text-white border border-[#505459]"
-          style={{
-            background: `linear-gradient(90deg, rgba(136, 158, 175, 0.8) 0%, rgba(27, 30, 32, 0.744) 98.32%)`,
-          }}
-          onClick={handleSubmit}
-        >
-          REGISTER
-        </div>
-      </div>
+          <div>
+            <button
+              className="btn btn-sm h-9 w-44 rounded-full text-white border border-[#505459]"
+              style={{
+                background: `linear-gradient(90deg, rgba(136, 158, 175, 0.8) 0%, rgba(27, 30, 32, 0.744) 98.32%)`,
+              }}
+              onClick={handleSubmit}
+            >
+              Continue
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
