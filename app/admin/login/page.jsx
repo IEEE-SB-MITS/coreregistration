4b1e5314415d4b320b4dccc091eb3d597e64ab16
Rohit George <code.mastermind.07@gmail.com>
2024-09-20 01:31:36 +0530

    fixes in admin page

--- app/admin/login/page.jsx
+++ app/admin/login/page.jsx
@@ -25,7 +25,7 @@ const Login = () => {
         <div className="flex justify-center items-center h-screen">
             <div className="w-full max-w-md">
                 <h1 className="text-2xl font-bold text-center mb-6">Admin Login</h1>
-                <form onSubmit={handleLogin} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <form onSubmit={handleLogin} className="bg-white  rounded px-8 pt-6 pb-8 mb-4">
                     {error && <p className="text-red-500 text-center mb-4">{error}</p>}
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
