557d87a650539010312e409d287c707856d9d7f5
Bhavan S <109290304+parzi-al@users.noreply.github.com>
2024-10-09 11:40:49 +0530

    newcore

--- app/components/CoreAnimation.jsx
+++ app/components/CoreAnimation.jsx
@@ -1,43 +1,43 @@
-import Image from 'next/image';
-import React from 'react';
+  import Image from 'next/image';
+  import React from 'react';
 
-const CoreAnimation = () => {
-  return (
-    <div className="w-full h-[90%] bg-black flex flex-col items-center justify-center">
-      
-      {/* Container with relative positioning */}
-      <div className="relative flex items-center justify-center w-full h-auto">
+  const CoreAnimation = () => {
+    return (
+      <div className="w-full h-3/4  bg-black flex flex-col items-center justify-center">
+ 
+        {/* Container with relative positioning */}
+        <div className="relative flex items-center justify-center w-full max-h-screen">
+          
+          {/* Background image (c2) */}
+          <Image 
+            src="/c2.png"
+            layout="intrinsic" 
+            width={600} 
+            height={600} 
+            objectFit="contain" 
+            alt="Background Image 2" 
+            className="absolute animate-spin-slow z-10" // Positioned relatively below c1
+            priority 
+          />
+
+          {/* Additional overlay image (c3) */}
+          
+
+          {/* Foreground image (c1) on top of the other two */}
+          <Image 
+            src="/c1.png" 
+            layout="intrinsic" 
+            width={600} 
+            height={600} 
+            objectFit="contain" 
+            alt="Foreground Image 1" 
+            className="absolute  z-30" // Positioned above both c2 and c3
+            priority 
+          />
+        </div>
         
-        {/* Background image (c2) */}
-        <Image 
-          src="/c2.png"
-          layout="intrinsic" 
-          width={600} 
-          height={600} 
-          objectFit="contain" 
-          alt="Background Image 2" 
-          className="absolute animate-spin-slow z-10" // Positioned relatively below c1
-          priority 
-        />
-
-        {/* Additional overlay image (c3) */}
-        
-
-        {/* Foreground image (c1) on top of the other two */}
-        <Image 
-          src="/c1.png" 
-          layout="intrinsic" 
-          width={600} 
-          height={600} 
-          objectFit="contain" 
-          alt="Foreground Image 1" 
-          className="absolute  z-30" // Positioned above both c2 and c3
-          priority 
-        />
       </div>
-      
-    </div>
-  );
-};
+    );
+  };
 
-export default CoreAnimation;
+  export default CoreAnimation;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import CoreAnimation from "./components/CoreAnimation";
 import Forms from "./components/forms";
+import { ShootingStars } from '@/components/ui/shooting-stars'; 
+import { StarsBackground } from '@/components/ui/stars-background';
 
 export default function Home() {
   return (
     <div className="h-screen w-screen flex">
-      <div className="w-1/2 h-full justify-center items-center md:block hidden">
+           <ShootingStars/>
+           <StarsBackground starDensity={0.001}/>
+      <div className="w-1/2  justify-center items-center md:block hidden">
         <CoreAnimation />
       </div>
       <div className="md:w-1/2 w-full h-full flex justify-center items-center">
